632c862802b8e3c83210099c6a62a3534202969f
EJ Mason <eliasjmason@gmail.com>
2020-02-17 19:44:25 -0800

    Hydrate data, write todo creator

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import TodoForm from "./TodoForm";
 import Todo from "./Todo";
 
+import uuid from "uuid";
+
 const DATA = [
   {
     id: "001",
@@ -12,7 +14,20 @@ const DATA = [
   { id: "002", name: "Oranges", checked: false, isEditing: true }
 ];
 
+function createTodo(name) {
+  return {
+    name,
+    id: uuid(),
+    checked: false
+  };
+}
+
 class App extends React.Component {
+  state = {
+    todos: DATA.map(
+      d => (d.isEditing = d.isEditing === undefined ? false : d.isEditing)
+    )
+  };
   render() {
     return (
       <div className="todoapp">
