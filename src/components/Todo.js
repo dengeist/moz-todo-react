fd736c8a4f6907a5dcb95d7f1f9f5ed0f6aef89d
EJ Mason <eliasjmason@gmail.com>
2020-02-17 20:10:12 -0800

    Convert Todo to class

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,46 +1,48 @@
 import React from "react";
 
-export default function Todo(props) {
-  const { id, name, checked, isEditing } = props;
-  const uniq = "todo-" + id;
-  return isEditing ? (
-    <li className="todo editing">
-      <div className="form-group">
-        <label className="todo-label" htmlFor={uniq}>
-          Rename {name}
-        </label>
-        <input id={uniq} className="todo-text" type="text" />
-      </div>
-      <div className="btn-group">
-        <button type="button" className="btn todo-cancel">
-          Cancel
-        </button>
-        <button type="button" className="btn btn__primary todo-edit">
-          Edit
-        </button>
-      </div>
-    </li>
-  ) : (
-    <li className="todo">
-      <div className="c-cb">
-        <input
-          id={uniq}
-          className="todo-toggle"
-          type="checkbox"
-          defaultChecked={checked}
-        />
-        <label className="todo-label" htmlFor={uniq}>
-          {name}
-        </label>
-      </div>
-      <div className="btn-group">
-        <button type="button" className="btn todo-edit">
-          Edit
-        </button>
-        <button type="button" className="btn btn__danger todo-delete">
-          Delete
-        </button>
-      </div>
-    </li>
-  );
+export default class Todo extends React.Component {
+  render() {
+    const { id, name, checked, isEditing } = this.props;
+    const uniq = "todo-" + id;
+    return isEditing ? (
+      <li className="todo editing">
+        <div className="form-group">
+          <label className="todo-label" htmlFor={uniq}>
+            Rename {name}
+          </label>
+          <input id={uniq} className="todo-text" type="text" />
+        </div>
+        <div className="btn-group">
+          <button type="button" className="btn todo-cancel">
+            Cancel
+          </button>
+          <button type="button" className="btn btn__primary todo-edit">
+            Edit
+          </button>
+        </div>
+      </li>
+    ) : (
+      <li className="todo">
+        <div className="c-cb">
+          <input
+            id={uniq}
+            className="todo-toggle"
+            type="checkbox"
+            defaultChecked={checked}
+          />
+          <label className="todo-label" htmlFor={uniq}>
+            {name}
+          </label>
+        </div>
+        <div className="btn-group">
+          <button type="button" className="btn todo-edit">
+            Edit
+          </button>
+          <button type="button" className="btn btn__danger todo-delete">
+            Delete
+          </button>
+        </div>
+      </li>
+    );
+  }
 }
