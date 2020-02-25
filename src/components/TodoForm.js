719d44568e266d3085ae997203e6b1de058df61e
EJ Mason <eliasjmason@gmail.com>
2020-02-25 14:45:17 -0800

    Add live region to form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { LiveAnnouncer, LiveMessage } from "react-aria-live";
 
 class TodoForm extends React.Component {
   state = {
-    todoText: ""
+    todoText: "",
+    errorMsg: ""
   };
 
   handleChange = e => {
@@ -11,7 +13,10 @@ class TodoForm extends React.Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    if (this.state.todoText === "") return;
+    if (this.state.todoText === "") {
+      this.setState({ errorMsg: "Your tasks cannot be blank." });
+      return;
+    }
 
     this.props.onCreateTodo(this.state.todoText);
     this.setState({ todoText: "" });
@@ -19,24 +24,27 @@ class TodoForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="new-todo-input" className="new-todo-label">
-          What needs to be done?
-        </label>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="new-todo-input input__fw"
-          name="text"
-          autoComplete="off"
-          value={this.state.todoText}
-          onChange={this.handleChange}
-          ref={this.props.newTodoInput}
-        />
-        <button type="submit" className="btn btn__primary new-todo-btn">
-          Add
-        </button>
-      </form>
+      <LiveAnnouncer>
+        <LiveMessage message={this.state.errorMsg} aria-live="polite" />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo-input" className="new-todo-label">
+            What needs to be done?
+          </label>
+          <input
+            type="text"
+            id="new-todo-input"
+            className="new-todo-input input__fw"
+            name="text"
+            autoComplete="off"
+            value={this.state.todoText}
+            onChange={this.handleChange}
+            ref={this.props.newTodoInput}
+          />
+          <button type="submit" className="btn btn__primary new-todo-btn">
+            Add
+          </button>
+        </form>
+      </LiveAnnouncer>
     );
   }
 }
