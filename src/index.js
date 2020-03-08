734c5bcfb343b47c9743eb7fef3cd54bfd0d3dcc
EJ Mason <eliasjmason@gmail.com>
2020-03-07 19:40:57 -0800

    Move todos to own file

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { LiveAnnouncer } from "react-aria-live";
 
 import "./index.css";
 import "./custom-checkboxes.css";
 
 import App from "./components/App";
 
-const TODOS = [
-  {
-    name: "Apples"
-  },
-  {
-    name: "Oranges"
-  },
-  {
-    name: "Tangerines"
-  }
-];
-
-ReactDOM.render(
-  <LiveAnnouncer>
-    <App />
-  </LiveAnnouncer>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
