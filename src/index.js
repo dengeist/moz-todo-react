49c2ebe0ba5bc1fc1f50717eb071fa5c1c87059a
EJ Mason <eliasjmason@gmail.com>
2020-03-07 19:31:08 -0800

    rename form control css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { LiveAnnouncer } from "react-aria-live";
 
 import "./index.css";
-import "./custom-form-controls.css";
+import "./custom-checkboxes.css";
 
 import App from "./components/App";
 
