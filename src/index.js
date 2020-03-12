4c2f39a94baf099e438763b4644d758cf13ffdb8
EJ Mason <eliasjmason@gmail.com>
2020-03-11 22:31:06 -0700

    Delete separate css file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import "./index.css";
-// import "./custom-checkboxes.css";
+import "./index.css";
 
 import App from "./components/App";
 
