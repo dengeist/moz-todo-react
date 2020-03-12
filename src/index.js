9d5a524fef78f1d33bcab53e706222e031b3a0a7
EJ Mason <eliasjmason@gmail.com>
2020-03-11 22:31:06 -0700

    Delete separate css file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-<<<<<<< HEAD
 import "./index.css";
-=======
-// import "./index.css";
-// import "./custom-checkboxes.css";
->>>>>>> add hidden text to triggers
 
 import App from "./components/App";
 
