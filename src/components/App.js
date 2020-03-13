044bf1c93b99ee192d895c4b239da939ab731af8
EJ Mason <eliasjmason@gmail.com>
2020-03-12 18:27:01 -0700

    Import components into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,19 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
 
 import React from "react";
+import Form from "./Form";
+import FilterButton from "./FilterButton";
 import Todo from "./Todo";
 
 function App(props) {
   const taskList = props.tasks.map(task => <Todo {...task} key={task.id} />);
   return (
     <div className="todoapp stack-large">
-      <form>
-        <h1>TodoMatic</h1>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all </span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>active </span>
-          <span className="visually-hidden">tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>completed </span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
