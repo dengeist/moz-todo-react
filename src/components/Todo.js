b59b48c315f9fbe62c1dc99df1227644cfbe4434
EJ Mason <eliasjmason@gmail.com>
2020-03-13 00:02:26 -0700

    Scaffold editing template

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,29 @@
 import React from "react";
 
 export default function Todo(props) {
-  return (
-    <li className="todo stack-small">
+  const isEditing = false;
+  const editingTemplate = (
+    <form class="stack-small">
+      <div className="form-group">
+        <label className="todo-label" htmlFor={props.id}>
+          New name for {props.name}
+        </label>
+        <input id={props.id} className="todo-text" type="text" />
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn todo-cancel">
+          Cancel
+          <span className="visually-hidden">renaming {props.name}</span>
+        </button>
+        <button type="submit" className="btn btn__primary todo-edit">
+          Save
+          <span className="visually-hidden">new name for {props.name}</span>
+        </button>
+      </div>
+    </form>
+  );
+  const defaultTemplate = (
+    <div className="stack-small">
       <div className="c-cb">
         <input id={props.id} type="checkbox" defaultChecked={props.completed} />
         <label className="todo-label" htmlFor={props.id}>
@@ -21,6 +42,9 @@ export default function Todo(props) {
           Delete <span className="visually-hidden">{props.name}</span>
         </button>
       </div>
-    </li>
+    </div>
+  );
+  return (
+    <li className="todo">{isEditing ? editingTemplate : defaultTemplate}</li>
   );
 }
