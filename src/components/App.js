454712b65f205bb6e8d50c907f382d4c6fbb555b
EJ Mason <eliasjmason@gmail.com>
2020-03-14 00:00:32 -0700

    Complete --> completed

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import Todo from "./Todo";
 const FILTER_MAP = {
   All: () => true,
   Active: task => !task.completed,
-  Complete: task => task.completed
+  Completed: task => task.completed
 };
 
 function App(props) {
